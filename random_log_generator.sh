f5c75301e290a8079ec161f73bba96343cea5640
Bharath Kumar N R <Bharath.Kumar.N.R@ibm.com>
2025-03-14 12:55:07 +0530

    Updated random_log_generator.sh file

--- random_log_generator.sh
+++ random_log_generator.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
 log_dir="/Users/bharathkumarnr/Intern_Automation1/randomlog"
-mkdir -p "$randomlog"
 while true
 do
-random_folder="$log_dir/$(date +%s%N)"
-mkdir -p "$random_folder"
-echo "Random log entry $(date)">"$random_folder/log.txt"
-sleep 10
+  current_timestamp=$(date +%d-%h-%Y_%H-%M-%S)
+  random_folder="$log_dir/$current_timestamp"
+  mkdir -p "$random_folder"
+  log_file="$random_folder/log$current_timestamp.txt"
+  echo "Log folder created at $random_folder" > "$log_file"
+  j=0
+  while [ $j -le 10 ]
+  do
+    echo "Random log entry $(date +%H%M%S)" >> "$log_file"
+    j=$((j+1))
+    sleep 5
+  done 
 done
